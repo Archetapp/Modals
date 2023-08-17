e5e3c046e99c274509058f9efa7069a1eff05b90
Jared Davidson (Archetapp) <archetapp@gmail.com>
2023-08-17 09:06:13 +0530

    update

--- Sources/Modals/ModalStack/ModalStackContainerView.swift
+++ Sources/Modals/ModalStack/ModalStackContainerView.swift
@@ -76,7 +76,7 @@ struct ModalStackContainerView<Content: View>: View, Equatable {
             }
             
             if ModalSystem.shared.isContentScalingEnabled {
-                contentScaleEffect = 0.92 + (0.08 * newValue)
+                contentScaleEffect = 0.95 + (0.08 * newValue)
                 contentCornerRadius = 36 + (UIScreen.main.displayCornerRadius - 36) * newValue
                 contentOffset = 30 - (30 * newValue)
             }
